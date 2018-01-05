f1de993d859fcb1437612c601364a2845c980b8d
JMH <jheld135@gmail.com>
2018-01-04 21:10:05 -0600

    Output options to user

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 'use strict';
 const alfy = require('alfy');
 
-alfy.output([
-	{
-		title: 'Unicorn',
-		subtitle: alfy.input
-	}
-]);
+const { parseRows } = require('./parser');
+
+alfy.fetch('https://learnxinyminutes.com', {
+	json: false,
+	transform: parseRows,
+	maxAge: 3600000
+}).then((rows) => {
+	const lowerCasedInput = alfy.input.toLowerCase();
+	const items = rows.filter((row) => row.name.toLowerCase().includes(lowerCasedInput))
+		.map((row) => ({
+			title: row.name,
+			arg: `https://learnxinyminutes.com/docs/${row.link}`
+		}));
+
+	alfy.output(items);
+});
+
