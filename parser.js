b3bd41d1b11c6dd394828b16b4cababed0fbc565
JMH <jheld135@gmail.com>
2018-01-04 19:52:46 -0600

    Add parseRows function

---
+++ parser.js
@@ -0,0 +1,41 @@
+'use strict';
+const jsdom = require('jsdom');
+const trim = require('lodash/trim');
+
+const { JSDOM } = jsdom;
+const virtualConsole = new jsdom.VirtualConsole();
+const defaultJsdomOptions = { virtualConsole };
+
+const linkStartIndex = '/docs/'.length;
+
+const parseRows = html => {
+  const { window: { document } } = new JSDOM(html, defaultJsdomOptions);
+
+  // Learnxinyminutes has multiple topics (not just programming languages!),
+  // each of which are separated into their own <table>
+  const tables = Array.from(document.querySelectorAll('table'));
+
+  // Collect all of the pages from within each topic group
+  return tables.reduce((pages, table) => {
+    return pages.concat(
+      Array.from(table.querySelectorAll('tr'))
+        // Exclude the first <tr> element as it is the <thead>;
+        .slice(1)
+        .map(row => {
+          // Get the page name
+          const link = row.querySelector('.name').firstElementChild;
+          return {
+            // Unfortunately JSDOM doesn't define innerText, so we need to use
+            // textContent and then trim whitespaces/newlines
+            name: trim(link.textContent, '\n '),
+            // Extract the URL param for this page, then remove the trailing '/'
+            link: link.href.slice(linkStartIndex).split('/')[0],
+          };
+        })
+    );
+  }, []);
+};
+
+module.exports = {
+  parseRows,
+};
