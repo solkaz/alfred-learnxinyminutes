8b9695d029e20b57d7d9ef90a0940331e73d93c9
Jeff Held <jh.software.developer@gmail.com>
2024-07-29 22:14:07 +0200

    Update workflow to support Alfy 2

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-'use strict';
-const alfy = require('alfy');
+import alfy from 'alfy';
+import { parseRows } from './parser.js';
 
-const { parseRows } = require('./parser');
-
-alfy.fetch('https://learnxinyminutes.com', {
-	json: false,
-	transform: parseRows,
-	maxAge: 3600000
-}).then((rows) => {
-	const lowerCasedInput = alfy.input.toLowerCase();
-	const items = rows.filter((row) => row.name.toLowerCase().includes(lowerCasedInput))
-		.map((row) => ({
-			title: row.name,
-			arg: `https://learnxinyminutes.com/docs/${row.link}`
-		}));
-
-	alfy.output(items);
+const data = await alfy.fetch('https://learnxinyminutes.com', {
+  json: false,
+  transform: parseRows,
+  maxAge: 3600000,
 });
 
+const items = alfy
+  .inputMatches(data, (item, input) => {
+    return item.name?.toLowerCase().includes(input.toLowerCase());
+  })
+  .map((row) => {
+    const link = `https://learnxinyminutes.com/docs/${row.link}`;
+    return {
+      title: row.name,
+      subtitle: link,
+      arg: link,
+      valid: true,
+    };
+  });
+
+alfy.output(items);
--- parser.js
+++ parser.js
@@ -1,18 +1,13 @@
-'use strict';
-const jsdom = require('jsdom');
-
-const { JSDOM } = jsdom;
-const virtualConsole = new jsdom.VirtualConsole();
-const defaultJsdomOptions = { virtualConsole };
+import * as cheerio from 'cheerio';
 
 const linkStartIndex = '/docs/'.length;
 
-const parseRows = html => {
-  const { window: { document } } = new JSDOM(html, defaultJsdomOptions);
+export const parseRows = (html) => {
+  const $ = cheerio.load(html);
 
   // Learnxinyminutes has multiple topics (not just programming languages!),
   // each of which are separated into their own <table>
-  const tables = Array.from(document.querySelectorAll('table'));
+  const tables = $('table');
 
   // Collect all of the pages from within each topic group
   return tables.reduce((pages, table) => {
@@ -20,21 +15,17 @@ const parseRows = html => {
       Array.from(table.querySelectorAll('tr'))
         // Exclude the first <tr> element as it is the <thead>;
         .slice(1)
-        .map(row => {
+        .map((row) => {
           // Get the page name
           const link = row.querySelector('.name').firstElementChild;
           return {
             // Unfortunately JSDOM doesn't define innerText, so we need to use
             // textContent and then trim whitespaces/newlines
-            name: (link.textContent || '').trim(),
+            name: (link?.textContent || '').trim(),
             // Extract the URL param for this page, then remove the trailing '/'
-            link: link.href.slice(linkStartIndex).split('/')[0],
+            link: link?.href?.slice(linkStartIndex).split('/')[0],
           };
         })
     );
   }, []);
-};
-
-module.exports = {
-  parseRows,
 };
